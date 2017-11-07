d5dfdc9d18c59e9fb7523a7d5b63fbe636548782
wanggangcanlang@sina.com <wanggangcanlang@sina.com>
2017-11-07 15:51:09 +0800

    双向数据绑定完结

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
+  private imgUrl = 'http://placehold.it/400x200';
+  private tom = 'tom';
+  private json = 'json111';
+  private cat = 'cat';
+  private disabledjyoutai = false;
   constructor() { }
 
   ngOnInit() {
@@ -14,5 +19,9 @@ export class BindComponent implements OnInit {
   doOnClick(event) {
     console.log(event);
   }
-
+  doOnInput(event) {
+    console.log(event.target.value);
+    console.log(event.target.getAttribute('value'));
+    this.json = event.target.value;
+  }
 }
