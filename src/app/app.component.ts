1b80e8bc4d1dcab12235263316103cf3884307f1
wanggangcanlang@sina.com <wanggangcanlang@sina.com>
2017-11-09 18:15:10 +0800

    6-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WjInputModule } from 'wijmo/wijmo.angular2.input';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+constructor(){
+  // let elementGoogle = document.querySelector('[title="クリックすると Google マップでこの地域を表示します"]');
+  // let elementRiyou = document.querySelector('[href="http://wijmo.com/products/wijmo-5/eval/        "]');
+  // elementRiyou['style'].display = 'none';
+  // elementGoogle.attr('tabindex', '-1');
+  // elementRiyou.attr('tabindex', '-1');
+
+}
+
+  private value: number = 123;
+  change() {
+    alert("my WjInputNumber value is " + this.value);
+  }
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BindComponent } from './bind/bind.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MultipePipe } from './pipe/multipe.pipe';
+import { WjInputModule } from 'wijmo/wijmo.angular2.input';
+import { StockComponent } from './stock/stock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindComponent
+    BindComponent,
+    MultipePipe,
+    StockComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    WjInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
